66874b8227992ae3c5a057cefafc63af26970d69
Cody Bumgardner <cody@uky.edu>
2018-07-15 17:39:23 -0400

    dash

--- src/main/java/io/cresco/dashboard/Activator.java
+++ src/main/java/io/cresco/dashboard/Activator.java
@@ -17,7 +17,6 @@ public class Activator implements BundleActivator {
 
         System.setProperty("org.jboss.logging.provider", "slf4j");
 
-
         /*
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
