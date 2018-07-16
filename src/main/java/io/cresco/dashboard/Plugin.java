393e205ad26cf957adf98d04a93a895ed442aa8c
Cody Bumgardner <cody@uky.edu>
2018-07-16 15:42:38 -0400

    blah

--- src/main/java/io/cresco/dashboard/Plugin.java
+++ src/main/java/io/cresco/dashboard/Plugin.java
@@ -46,64 +46,13 @@ public class Plugin implements PluginService {
     //private HttpService server;
     public String repoPath = null;
     private ConfigurationAdmin configurationAdmin;
-
+    private Map<String,Object> map;
 
     @Activate
     void activate(BundleContext context, Map<String,Object> map) {
 
         this.context = context;
-
-
-        try {
-            //this will be called twice due to JAX-RS-Connector
-            if(pluginBuilder == null) {
-                pluginBuilder = new PluginBuilder(this.getClass().getName(), context, map);
-                this.logger = pluginBuilder.getLogger(Plugin.class.getName(), CLogger.Level.Info);
-                this.executor = new PluginExecutor(pluginBuilder);
-                pluginBuilder.setExecutor(executor);
-
-                while (!pluginBuilder.getAgentService().getAgentState().isActive()) {
-                    logger.info("Plugin " + pluginBuilder.getPluginID() + " waiting on Agent Init");
-                    //System.out.println("Plugin " + pluginBuilder.getPluginID() + " waiting on Agent Init");
-                    Thread.sleep(1000);
-                }
-
-                pluginBuilder.setIsActive(true);
-
-            }
-            /*
-            logger.info("Plugin " + pluginBuilder.getPluginID() + " Setting Classes");
-
-            AuthenticationFilter.connectPlugin(pluginBuilder);
-            RootController.connectPlugin(pluginBuilder);
-            AlertsController.connectPlugin(pluginBuilder);
-            AgentsController.connectPlugin(pluginBuilder);
-            PluginsController.connectPlugin(pluginBuilder);
-            RegionsController.connectPlugin(pluginBuilder);
-            GlobalController.connectPlugin(pluginBuilder);
-            ApplicationsController.connectPlugin(pluginBuilder);
-
-            logger.info("Plugin " + pluginBuilder.getPluginID() + " Classes Set");
-            */
-
-            /*
-            try {
-                repoPath = getRepoPath();
-                server = startServer("http://[::]:" + pluginBuilder.getConfig().getStringParam("port", "3445") + "/");
-            } catch(Exception ex) {
-                ex.printStackTrace();
-                server = startServer("http://0.0.0.0:" + pluginBuilder.getConfig().getStringParam("port", "3445") + "/");
-                //logger.error("failed : " + ex.getMessage());
-            }
-            */
-
-            //set plugin active
-
-
-
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
+        this.map = map;
 
     }
 
@@ -147,5 +96,30 @@ public class Plugin implements PluginService {
         return path;
     }
 
+    @Override
+    public boolean isStarted() {
+        try {
+            //this will be called twice due to JAX-RS-Connector
+            if(pluginBuilder == null) {
+                pluginBuilder = new PluginBuilder(this.getClass().getName(), context, map);
+                this.logger = pluginBuilder.getLogger(Plugin.class.getName(), CLogger.Level.Info);
+                this.executor = new PluginExecutor(pluginBuilder);
+                pluginBuilder.setExecutor(executor);
 
+                while (!pluginBuilder.getAgentService().getAgentState().isActive()) {
+                    logger.info("Plugin " + pluginBuilder.getPluginID() + " waiting on Agent Init");
+                    //System.out.println("Plugin " + pluginBuilder.getPluginID() + " waiting on Agent Init");
+                    Thread.sleep(1000);
+                }
+
+                pluginBuilder.setIsActive(true);
+
+            }
+            return true;
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 }
